b082ae18ef0882b6eab5c6f11c555efea1b8309d
jacob fraizer <jacob33c@gmail.com>
2019-08-26 02:28:06 -0700

    initial commit

---
+++ paid-by-the-minute/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  paid-by-the-minute
+//
+//  Created by Jacob Fraizer on 8/25/19.
+//  Copyright © 2019 Jacob Fraizer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    var timer = Timer()
+    var duration = 0
+    var total : Float = 0.00
+    var timerOn = false
+    
+    
+    @IBOutlet weak var moneyLabel: UILabel!
+    
+    @IBOutlet weak var hourlyRate: UITextField!
+    
+    @IBOutlet weak var startButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    func toggleTimer(on: Bool, increment: Float){
+        if on == false{
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+                let twoDecimalPlaces = String(format: "%.2f", self.total)
+                self.moneyLabel.text = "$ \(twoDecimalPlaces)"
+                self.total += increment
+                print("increment = \(increment)")
+                print("total = $\(self.total) ")
+                self.timerOn = true
+                self.startButton.backgroundColor = UIColor.red
+                self.startButton.setTitle("Stop", for: .normal)
+                self.hourlyRate.isHidden = true
+            })
+        }
+        else{
+            self.startButton.backgroundColor = UIColor.green
+            self.startButton.setTitle("Start", for: .normal)
+            timer.invalidate()
+            self.timerOn = false
+            self.hourlyRate.isHidden = false
+        }
+    }
+    
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        
+        if let rate = hourlyRate.text{
+            if let rateNum = Float(rate){
+                hourlyRate.resignFirstResponder()
+                print("rate = \(rate)")
+                print("rateNum = \(rateNum)")
+                let increment: Float = Float(rateNum / 3600)
+                toggleTimer(on: timerOn, increment: increment)
+            }
+        }
+    
+    }
+    
+    @IBAction func restartButtonPressed(_ sender: Any) {
+        self.total = 0.00
+        self.moneyLabel.text = "$0.00"
+        self.timerOn = false
+        self.startButton.backgroundColor = UIColor.green
+        self.startButton.setTitle("Start", for: .normal)
+        timer.invalidate()
+        self.timerOn = false
+        self.hourlyRate.text = ""
+        self.hourlyRate.isHidden = false
+
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        hourlyRate.resignFirstResponder()
+        return true
+    }
+    
+    
+}
+       
+
+    
+    
+
+
---
+++ paid-by-the-minute/done-button.swift
@@ -0,0 +1,43 @@
+//
+//  done-button.swift
+//  paid-by-the-minute
+//
+//  Created by Jacob Fraizer on 8/26/19.
+//  Copyright © 2019 Jacob Fraizer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
